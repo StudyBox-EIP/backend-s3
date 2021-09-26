6ed2a12bc55de0d7e5e83d09a8f3e66a4eeac4e3
BLARD <justin.blard@epitech.eu>
2021-09-26 13:08:52 +0100

    ADD: Argparse Implemented
    Argparse as been implemented in this own file.
    Some file have been cut to make it easier to read.
    requirements have been updated to have argparse.
    makefile as been updated to follow name sake.
    __main__.py as been updated to make it easier to read.
    comments have been added to arparse and main functions.

--- src/__main__.py
+++ src/__main__.py
@@ -1,59 +1,22 @@
 #!/usr/bin/env python3
-import numpy as np
-import cv2 as cv
+from addons.arguments import treat_arguments
+from addons.work import camera, video
 from sys import argv
 from typing import List
-from os.path import exists
 
 
-def video(path: str) -> int:
-    cap = cv.VideoCapture(path)
-    while cap.isOpened():
-        ret, frame = cap.read()
-        # if frame is read correctly ret is True
-        if not ret:
-            print("Can't receive frame (stream end?). Exiting ...")
-            break
-        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-        cv.imshow("frame", gray)
-        if cv.waitKey(1) == ord("q"):
-            break
-    cap.release()
-    cv.destroyAllWindows()
-    return 0
+def main() -> int:
+    """
+    This function starts the program.
+    """
+    elm = treat_arguments(argv)
 
-
-def camera() -> int:
-    cap = cv.VideoCapture(0)
-    if not cap.isOpened():
-        print("Cannot open camera")
-        return 84
-    while cap.isOpened():
-        # Capture frame-by-frame
-        ret, frame = cap.read()
-        # if frame is read correctly ret is True
-        if not ret:
-            print("Can't receive frame (stream end?). Exiting ...")
-            break
-        # Our operations on the frame come here
-        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-        # Display the resulting frame
-        cv.imshow("frame", gray)
-        if cv.waitKey(1) == ord("q"):
-            break
-    # When everything done, release the capture
-    cap.release()
-    cv.destroyAllWindows()
-    return 0
-
-
-def main(n: int, args: List[str]) -> int:
-    if n == 2 and args[1] == "cam":
+    if elm[0] == "cam":
         return camera()
-    elif n == 3 and args[1] == "vid" and exists(args[2]):
-        return video(args[2])
+    elif elm[0] == "vid":
+        return video(elm[1])
     return 84
 
 
 if __name__ == "__main__":
-    exit(main(len(argv), argv))
+    exit(main())
---
+++ src/addons/arguments.py
@@ -0,0 +1,58 @@
+from typing import Tuple, List
+from argparse import ArgumentParser, ArgumentError
+from pathlib import Path
+from sys import stderr
+from os.path import isfile
+
+# Default video when 'vid' type is selected
+default_video = ""
+
+# This is all format video accepted by the program
+video_format = [".mp4", ".m4a"]
+
+
+def parse_arguments(arguments: List[str]) -> Tuple[str, str]:
+    """
+    This fonction parses all the arguments and takes care of the help display.
+    This returns a tuple of all the wanted arguement.
+    It should takes 'argv' without the first element.
+    """
+    parser = ArgumentParser()
+    parser.add_argument(
+        "type",
+        type=str,
+        choices=["cam", "vid"],
+        help="this is the type of use wanted for the program",
+    )
+    parser.add_argument(
+        "-f",
+        "--file",
+        type=str,
+        default=default_video,
+        help="this is the video used by the 'vid' type",
+    )
+    try:
+        args = parser.parse_args(arguments)
+    except ArgumentError:
+        parser.print_help()
+        exit(84)
+    return (args.type, args.file)
+
+
+def treat_arguments(args: List[str]) -> Tuple[str, str]:
+    """
+    This takes in all the argument wanted an treats them.
+    This returns a Tuple with the functionment type and the adress of a video file.
+    This function exits when a parameter is not right or as an issue.
+    """
+    parse = parse_arguments(args[1:])
+
+    if parse[0] == "vid":
+        if not isfile(parse[1]):
+            print("Adress given is not a file or doesn't exist.", file=stderr)
+            exit(84)
+        if not (parse[1][-4:] in video_format):
+            print("This file is not in an accepted format.", file=stderr)
+            print(f"The accepted format are : {video_format}", file=stderr)
+            exit(84)
+    return (parse[0], parse[1])
