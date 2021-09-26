1352dfe75719df9495655c1e933f005f91f67052
BLARD <justin.blard@epitech.eu>
2021-09-26 13:51:35 +0100

    ADD: Basic Rabbit video
    Add the basic mp4 video.
    Update test to include the file.
    Arguments takes it as default option.

--- tests/test_1.py
+++ tests/test_1.py
@@ -4,6 +4,6 @@ import cv2 as cv
 
 
 def test_dependence_good() -> None:
-    capture = cv.VideoCapture("./video/SampleVideo_1280x720_1mb.mp4")
+    capture = cv.VideoCapture("assets/videos/rabbit.mp4")
     assert capture.isOpened()
     capture.release()
