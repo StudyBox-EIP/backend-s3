88ca1c20e84e7602f481aff6cfed9863a20b9049
BLARD <justin.blard@epitech.eu>
2021-10-06 22:19:12 +0100

    git-annex in vids

--- tests/test_check_pictures.py
+++ tests/test_check_pictures.py
@@ -1,5 +1,1 @@
-from src.check_pictures import check_pedestrian
-
-
-def test_pictures_normal() -> None:
-    assert check_pedestrian(50) == 0
+/annex/objects/SHA256E-s123--55bfb7a79b5f77ddbe6bf010ed2385df82a6dbfa38b8422d331651d9b116b778.py
